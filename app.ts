89a5200c63e321fc1266611f439213bb495dd9a9
Geoffrey Prest (CGI) <geoffrey.prest@cgi.com>
2018-11-08 09:58:12 -0500

    Variables and type annotations

--- app.ts
+++ app.ts
@@ -1,7 +1,44 @@
-class HelloWorld {
-    constructor(public message: string) {}
+function getAllBooks() {
+  const books = [
+    { 
+      title: 'Ulysses',
+      author: 'James Joyce',
+      available: true },
+    {
+      title: 'A Farewell to Arms',
+      author: 'Ernest Hemingway',
+      available: false
+    },
+    {
+      title: 'I Know Why the Caged Bird Sings',
+      author: 'Maya Anglou',
+      available: true
+    },
+    {
+      title: 'Moby Disk', 
+      author: 'Herman Melville', 
+      available: true 
+    }
+  ];
+
+  return books;
 }
 
-const hello = new HelloWorld('Hello TypeScript');
-console.log(hello.message);
-console.log('Hello Visual Studio Code');
+function logFirstAvailable(books: any): void {
+  let firstAvailable: string = '';
+  let numberOfBooks: number = books.length;
+
+  for (const currentBook of books) {
+    if (currentBook.available) {
+      firstAvailable = currentBook.title;
+      break;
+    }
+  }
+
+  console.log(`Total Books: ${numberOfBooks}`);
+  console.log(`First Available: ${firstAvailable}`);
+}
+
+const allBooks = getAllBooks();
+allBooks.push();
+logFirstAvailable(allBooks);
